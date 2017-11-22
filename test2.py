b8eff19c81bd1d39070861169a747ea8d6c5dc91
eddie-cheung <eddie_zhang@outlook.com>
2017-11-22 10:47:22 +0800

    Update test2.py

--- test2.py
+++ test2.py
@@ -7,8 +7,8 @@ browser = webdriver.Chrome()
 browser.get("https://weibo.com/")
 
 time.sleep(5)
-browser.find_element_by_name("username").send_keys("xhzhang44555@sina.com")
-browser.find_element_by_name("password").send_keys("Zhangxuhui1991")
+browser.find_element_by_name("username").send_keys("account")
+browser.find_element_by_name("password").send_keys("password")
 browser.find_element_by_xpath("//div[@class='info_list login_btn']/a[@node-type='submitBtn']").click()
 
 print browser.page_source
